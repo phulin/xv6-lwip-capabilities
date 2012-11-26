fdd606b98969d5ca6be5d879adb634d8534753df
Patrick Hulin <patrick.hulin@gmail.com>
2012-11-26 02:16:51 -0500

    made grader a more real demo

--- grader.c
+++ grader.c
@@ -13,7 +13,10 @@ int main(void)
 {
   int fd;
   char old[] = "Patrick: A\nSteven: A\n";
-  char new[] = "Patrick: B\nSteven: A+\n";
+  char new1[] = "Patrick: B\nSteven: A+\n";
+  char new2[] = "Patrick: A+\nSteven: B\n";
+  char *new = new1;
+  char buf[512];
 
   fd = open("grades.txt", O_CREATE | O_RDWR);
   if (fd < 0) {
@@ -21,7 +24,22 @@ int main(void)
     exit();
   }
   write(fd, old, sizeof(old));
+  close(fd);
+  fd = open("grades.txt", O_CREATE | O_RDWR);
+  if (fd < 0) {
+    printf(1, "open failed: %e", fd);
+    exit();
+  }
+  int n = read(fd, buf, sizeof(old));
+  buf[n] = '\0';
+  printf(1, "grades.txt:\n%s", buf);
+  close(fd);
 
+  fd = open("grades.txt", O_CREATE | O_RDWR);
+  if (fd < 0) {
+    printf(1, "open failed: %e", fd);
+    exit();
+  }
   cap_enter();
   int pid = fork();
   if (pid == 0) {
@@ -29,9 +47,13 @@ int main(void)
 
     close(fd);
     fd = newfd;
+    new = new2;
   }
 
-  if (write(fd, new, sizeof(new)) > 0) {
+  cap_rights_t rights;
+  cap_getrights(fd, &rights);
+  printf(1, "\nTrying to write [caps %d]:\n%s", rights, new);
+  if (write(fd, new, sizeof(new1)) > 0) {
     printf(1, "Write succeeded on pid %d\n", pid);
   } else {
     printf(1, "Write failed on pid %d\n", pid);
@@ -39,8 +61,19 @@ int main(void)
 
   close(fd);
 
-  if (pid != 0)
+  if (pid != 0) {
     wait();
+
+    fd = open("grades.txt", O_CREATE | O_RDWR);
+    if (fd < 0) {
+      printf(1, "open failed: %e", fd);
+      exit();
+    }
+    int n = read(fd, buf, sizeof(old));
+    buf[n] = '\0';
+    printf(1, "\nnew grades.txt:\n%s", buf);
+    close(fd);
+  }
 
   exit();
 }
